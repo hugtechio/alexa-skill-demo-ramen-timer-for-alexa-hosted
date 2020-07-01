daf0c515eacf81c8aea74761692814c0e181b94b
haruharuharuby <cocktail.house@gmail.com>
2020-07-01 21:48:20 +0200

    first commit

---
+++ lambda/index.js
@@ -0,0 +1,191 @@
+const Alexa = require('ask-sdk');
+const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+const talk = require('./talk')
+const timer = require('./timer')
+
+let storage = null
+
+// Synonym で定義したidをキーとして扱いたいので、slotの構造から直接取る。
+function getSynonymValues(handlerInput, key) {
+  try{
+    return Alexa.getSlot(handlerInput.requestEnvelope, key).resolutions.resolutionsPerAuthority[0].values[0].value.name.toLowerCase()
+  } catch (e) {
+    console.log(e)
+    return ''
+  }
+}
+
+// PersistentAttributesとSessionAttributesはHandlerが処理される前に必ずあるようにしたいので、
+// Interceptor に実装
+const RequestInterceptor = {
+  async process(handlerInput) {
+    console.log(handlerInput.requestEnvelope.request.intent)
+    const { attributesManager } = handlerInput;
+    try {
+      storage = await attributesManager.getPersistentAttributes() || {};
+    } catch (e) {
+      storage = {}
+    }
+    session = attributesManager.getSessionAttributes();
+
+    try {
+      if (Object.keys(session).length === 0) {
+        attributesManager.setSessionAttributes(session)      
+      }
+    } catch (error) {
+      console.log(error)
+      attributesManager.setSessionAttributes(session)  
+    }
+    console.log('storage:', storage)
+    console.log('session:', session)
+  }
+};
+
+// Attributesの保存は、handlerが呼ばれたあとの共通処理で実装(=ResponseInterceptorss)
+const ResponseInterceptor = {
+  async process(handlerInput) {
+    storage.visit = "1"
+    const { attributesManager } = handlerInput;
+    await attributesManager.savePersistentAttributes(storage);
+    attributesManager.setSessionAttributes(session);
+  }
+};
+
+function getPersistenceAdapter(tableName) {
+  return new ddbAdapter.DynamoDbPersistenceAdapter({
+    tableName: tableName,
+    createTable: true
+  });
+}
+
+const LaunchRequest = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  async handle(handlerInput) {
+    // Timer を使うには ユーザーの許可が必要。有効になってなければ声で促す
+    const directive = timer.verifyConsentToken(handlerInput)
+    if (directive) return talk.launch(handler.response, storage, timerAskDirective)
+
+    return talk.launch(handlerInput.responseBuilder, storage)
+  },
+};
+
+const AskForResponseHandler = {
+  canHandle(handlerInput) {
+      return Alexa.getRequestType(handlerInput.requestEnvelope) === 'Connections.Response'
+          && handlerInput.requestEnvelope.request.name === 'AskFor';
+  },
+  async handle(handlerInput) {
+    return timer.permissionCallback(handlerInput)
+  }
+}
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return talk.exit(handlerInput.responseBuilder)
+  },
+};
+
+const SessionEndedRequest = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const HelpIntent = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return talk.help(handlerInput.responseBuilder)
+  }
+};
+
+const UnhandledIntent = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput) {
+    return talk.unhandled(handlerInput.responseBuilder)
+  },
+};
+
+const SetNoodleTimerIntent = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SetNoodleTimerIntent'
+  },
+  async handle(handlerInput) {
+  
+    const noodle = getSynonymValues(
+      handlerInput, 'noodle'
+    )
+    const softy = getSynonymValues(
+      handlerInput, 'softy'
+    )
+    const minutes = getSynonymValues(
+      handlerInput, 'minutes'
+    )
+
+    console.log(noodle, softy, minutes)
+    sessionAttributesOrError = await timer.runTimer(
+      handlerInput, 
+      noodle, (minutes) ? minutes : softy)
+    
+    return talk.SetNoodleTimerIntent(
+      handlerInput.responseBuilder,
+      sessionAttributesOrError)
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+
+    return talk.error(handlerInput.responseBuilder, session.diagnosisAttributes)
+  },
+};
+
+const FallbackHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent');
+  },
+  handle(handlerInput) {
+    return talk.fallback(handlerInput.responseBuilder)
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+exports.handler = skillBuilder
+  .withPersistenceAdapter(getPersistenceAdapter('AlexaSkillDemoRamenTimer'))
+  .addRequestHandlers(
+    LaunchRequest,
+    ExitHandler,
+    SessionEndedRequest,
+    SetNoodleTimerIntent,
+    HelpIntent,
+    FallbackHandler,
+    UnhandledIntent,
+  )
+  .addRequestInterceptors(RequestInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .addResponseInterceptors(ResponseInterceptor)
+  /** API 呼び出すクライアントを使う宣言 */
+  .withApiClient(new Alexa.DefaultApiClient())
+  .lambda();
---
+++ lambda/timer.js
@@ -0,0 +1,138 @@
+
+const TIMERS_PERMISSION = 'alexa::alerts:timers:skill:readwrite'
+
+const timerConfig = {
+    noodle: {
+        'ラ王': 3,
+        'どん兵衛': 5
+    },
+    softy: {
+        'かため': -1,
+        'やわらかめ': 1
+    }
+}
+
+/**
+ * duration の文字列形式 ISO8601 継続期間にコンバートする
+ * @param {*} noodleName カップ麺の名前
+ * @param {*} softyOrMinutes 麺のかたさ or 分
+ */
+const getAlexaDuration = (noodleName, softyOrMinutes) => {
+    console.log(noodleName, softyOrMinutes)
+    const extra = parseInt(softyOrMinutes)
+    const duration = (isNaN(extra))
+        ? timerConfig.noodle[noodleName] + timerConfig.softy[softyOrMinutes]
+        : timerConfig.noodle[noodleName] + extra
+    const durationString = `PT${duration}M`
+    console.log('Duration:', durationString)
+    return durationString
+}
+
+const message = {
+    locale: 'ja-JP',
+    label: (noodle) => `${noodle} ができるまであと...`,
+    done: (noodle) => { 
+        return `${noodle} ができました。冷めないうちにどうぞ！`
+    }
+}
+
+const getTimerTemplate = (noodle, softyOrMinutes) => {
+    return {
+        duration: getAlexaDuration(noodle, softyOrMinutes),
+        label: message.label(noodle, softyOrMinutes),
+        creationBehavior: {
+            displayExperience: {
+                visibility: 'VISIBLE'
+            }
+        },
+        triggeringBehavior: {
+            operation: {
+                type : 'ANNOUNCE',
+                textToAnnounce: [{
+                    locale: 'ja-JP',
+                    text: message.done(noodle)
+                }]
+            },
+            notificationConfig: {
+                playAudible: true
+            }
+        }
+    }
+}
+
+/**
+ * Timer が許可されてなかったら、許可してもいいかユーザーに尋ねる
+ * @param {*} handlerInput
+ * @returns 許可されてなかったら、許可を尋ねるレスポンスを返す
+ * 許可されてれば nullを。
+ */
+module.exports = {
+    verifyConsentToken: (handlerInput) => {
+        let {requestEnvelope} = handlerInput;
+        const {permissions} = requestEnvelope.context.System.user;
+        if (!(permissions && permissions.consentToken)){
+            console.log('No permissions found!');
+            return {
+                type: 'Connections.SendRequest',
+                'name': 'AskFor',
+                'payload': {
+                    '@type': 'AskForPermissionsConsentRequest',
+                    '@version': '1',
+                    'permissionScope': TIMERS_PERMISSION
+                },
+                token: 'verifier'
+            }
+        }
+        console.log('Permissions found: ' + permissions.consentToken);
+        return null;
+    },
+    /**
+     * Timerを有効にしたかどうかの応答状況を返す
+     * @param {} handlerInput 
+     */
+    permissionCallback: (handlerInput) => {
+        console.log('Handler: AskForResponseHandler');
+        const {request} = handlerInput.requestEnvelope;
+        const {payload, status} = request;
+        console.log('Connections response status + payload: ' + status + ' - ' + JSON.stringify(payload));
+
+        return {
+            code: status.code,
+            status: payload.status,
+            isCardThrown: payload.isCardThrown
+        }
+    },
+    runTimer: async (handlerInput, noodle, softy) => {
+        const {attributesManager, serviceClientFactory} = handlerInput;
+    
+        try {
+            const timerServiceClient = serviceClientFactory.getTimerManagementServiceClient();
+            const timerResponse = await timerServiceClient.createTimer(
+                getTimerTemplate(noodle, softy)
+            )
+    
+            const timerId = timerResponse.id;
+            const timerStatus = timerResponse.status;
+            console.log(timerResponse)
+
+            const sessionAttributes = attributesManager.getSessionAttributes();
+            sessionAttributes['lastTimerId'] = timerId
+            sessionAttributes['noodle'] = noodle
+            sessionAttributes['softy'] = softy
+            if(timerStatus === 'ON') {
+                sessionAttributes['lastTimerStatus'] = timerStatus
+                return sessionAttributes
+            } else {
+                sessionAttributes['error'] = 308 
+                sessionAttributes['errorKey'] = 'TIMER_DID_NOT_START'
+                return sessionAttributes
+            }
+        } catch (e) {
+            console.log(e)
+            sessionAttributes['error'] = e.statusCode
+            sessionAttributes['errorKey'] = 'TIMER_DID_NOT_START'
+            return sessionAttributes
+        }
+    }
+}
+
